f166463e00964a32636a87505a57c95b5dc7c5f9
Shreyansh Srivastava <shreyansh.official.6726@gmail.com>
2026-06-10 00:07:21 +0530

    work

--- frontend/farmer-portal/src/config.js
+++ frontend/farmer-portal/src/config.js
@@ -1,16 +1,13 @@
 const rawBackendUrl =
   process.env.REACT_APP_BACKEND_APP_URL ||
-  process.env.REACT_BACKEND_APP_URL ||
   'http://localhost:5000';
 
 export const BACKEND_URL = rawBackendUrl.replace(/\/$/, '');
 export const GROQ_CHATBOT_API =
   process.env.REACT_APP_GROQ_CHATBOT_API ||
-  process.env.GROQ_CHATBOT_API ||
   '';
 export const BACKEND_API_KEY =
   process.env.REACT_APP_BACKEND_API_KEY ||
-  process.env.BACKEND_API_KEY ||
   '';
 
 export function apiUrl(path = '') {
--- frontend/wholesaler-portal/src/config.js
+++ frontend/wholesaler-portal/src/config.js
@@ -1,12 +1,10 @@
 const rawBackendUrl =
   process.env.REACT_APP_BACKEND_APP_URL ||
-  process.env.REACT_BACKEND_APP_URL ||
   'http://localhost:5000';
 
 export const BACKEND_URL = rawBackendUrl.replace(/\/$/, '');
 export const BACKEND_API_KEY =
   process.env.REACT_APP_BACKEND_API_KEY ||
-  process.env.BACKEND_API_KEY ||
   '';
 
 export function apiUrl(path = '') {
